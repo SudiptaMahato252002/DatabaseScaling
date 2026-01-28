f44add79da1d56c460b8a077b3dae9f8c08e3977
SudiptaMahato252002 <sudipmaha123@gmail.com>
2026-01-28 09:27:26 +0530

    Create two shards and initailise the tables in them

---
+++ src/main/java/com/example/DatabaseScaling/DatabaseScalingApplication.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseScaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseScalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseScalingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DatabaseScaling/DatabaseScalingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseScaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseScalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
